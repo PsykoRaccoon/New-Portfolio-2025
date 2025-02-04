8a1ca1f2bb3b950212a2106c79a47308acdf6c10
Victor Sanchez <vickoss9004@gmail.com>
2025-02-03 23:57:03 -0600

    Aesthetics

---
+++ docs/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll(".fade-in");
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.3 }); 
+  
+    fadeElements.forEach(el => observer.observe(el));
+  });
+  
